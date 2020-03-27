974b2d0066605d0c04d644d418ac9fa5648b17c6
Mohit <mohit.kumar5037@gmail.com>
2020-03-27 11:27:11 +0530

    We can create Game Objects and put them out of core data

---
+++ GameCollector/GameViewController.swift
@@ -0,0 +1,55 @@
+//
+//  GameViewController.swift
+//  GameCollector
+//
+//  Created by Mohit on 27/03/20.
+//  Copyright © 2020 Practise. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var gameImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagePicker.delegate = self
+    }
+    
+
+    @IBAction func photosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        gameImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let game = Game(context: context)
+        game.title = titleTextField.text!
+        game.image = gameImageView.image!.pngData()
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+        
+    }
+}
